9caa66c7018ee03b71c622958dc2e18d983ed233
Jesse McReady <jessemcready@gmail.com>
2018-10-23 11:41:43 -0400

    login and create user now sends back json of current user, their groups, and those groups' users

--- lazy-friends/app/controllers/users_controller.rb
+++ lazy-friends/app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
   def create
     @user = User.find_or_create_by(user_params)
     if @user.save
-      render json: @user, status: :created
+      render json: { user: @user.as_json(include: { groups: { include: { users: { except: :password } } } } ) }
     else
       render json: {status: "error", code: 3000, message: @user.errors}
     end
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
     @user = User.find_by(username: user_params[:username])
     if @user
       if @user.password == user_params[:password]
-        render json: @user, except: :password, status: :ok
+        render json: { user: @user.as_json(include: { groups: { include: { users: { except: :password } } } } ) }
       else
         render json: { status: 'error', code: '3100', message: 'Username or password invalid'}
       end
