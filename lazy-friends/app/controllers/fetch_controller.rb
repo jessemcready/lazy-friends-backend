5df7d81ab66d30170816ec24c98eefcb39076ead
Jesse McReady <jessemcready@gmail.com>
2018-10-25 10:52:45 -0400

    fetch places

--- lazy-friends/app/controllers/fetch_controller.rb
+++ lazy-friends/app/controllers/fetch_controller.rb
@@ -4,7 +4,8 @@ class FetchController < ApplicationController
 
   def places
     coordinates = params[:coordinates]
-    @response = RestClient.get 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=40.740276,-73.972148&radius=750&type=bar&key=AIzaSyAIYICLeEdYXE_PCKhve_JNFWbqrNL3OD0'
+    # byebug
+    @response = RestClient.get "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{coordinates}&radius=750&type=bar&key=AIzaSyAIYICLeEdYXE_PCKhve_JNFWbqrNL3OD0"
     render json: @response, status: :ok
   end
 
