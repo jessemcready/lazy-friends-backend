326bd58d13e848f6ba53c667197378fcb516f97f
ParkMD7 <parkermdinsmore@gmail.com>
2018-11-29 14:18:20 -0500

    added google api key

--- lazy-friends/app/controllers/fetch_controller.rb
+++ lazy-friends/app/controllers/fetch_controller.rb
@@ -4,8 +4,9 @@ class FetchController < ApplicationController
 
   def places
     coordinates = params[:coordinates]
-    # byebug
-    @response = RestClient.get "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{coordinates}&radius=750&type=bar&key=#{your-api-key}"
+    api_key = Rails.application.credentials.google_api_key
+    
+    @response = RestClient.get "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{coordinates}&radius=750&type=bar&key=#{api_key}"
     render json: @response, status: :ok
   end
 
