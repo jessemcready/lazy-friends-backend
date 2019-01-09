6f1f692fe8dc28d82b5f6222488c20862a970397
Jesse McReady <jessemcready@gmail.com>
2019-01-09 16:57:08 -0500

    started moving from google places to yelp

--- lazy-friends/app/controllers/fetch_controller.rb
+++ lazy-friends/app/controllers/fetch_controller.rb
@@ -4,10 +4,13 @@ class FetchController < ApplicationController
   skip_before_action :authorized, only: [:places]
 
   def places
-    coordinates = params[:coordinates]
-    api_key = Rails.application.credentials.google_api_key
-
-    @response = RestClient.get "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{coordinates}&radius=750&type=bar&key=#{api_key}"
+    coordinates = params[:coordinates].split(',')
+    lat = coordinates[0]
+    long = coordinates[1]
+    term = params[:suggestions]
+    api_key = Rails.application.credentials.yelp_api_key
+    byebug
+    @response = RestClient.get "https://api.yelp.com/v3/businesses/search?term=food&latitude=#{lat}&longitude=#{long}&radius=750",  { :Authorization => "Bearer #{api_key}" }
     render json: @response, status: :ok
   end
 
