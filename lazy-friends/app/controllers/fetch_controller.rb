2d611d4d6dbd31621a61ece7a6a49f56a4ad1274
ParkMD7 <parkermdinsmore@gmail.com>
2019-01-10 15:04:43 -0500

    commented out a method in fetch_controller

--- lazy-friends/app/controllers/fetch_controller.rb
+++ lazy-friends/app/controllers/fetch_controller.rb
@@ -3,10 +3,17 @@ require 'rest-client'
 class FetchController < ApplicationController
   skip_before_action :authorized, only: [:places]
 
+  # This is used for the Google Places API which we have replaced with the Yelp API Below
+  # def places
+  #   coordinates = params[:coordinates]
+  #   api_key = Rails.application.credentials.google_api_key
+  #   @response = RestClient.get "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{coordinates}&radius=750&type=bar&key=#{api_key}"
+  #   render json: @response, status: :ok
+  # end
+
   def places
     coordinates = params[:coordinates]
     api_key = Rails.application.credentials.google_api_key
-
     @response = RestClient.get "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{coordinates}&radius=750&type=bar&key=#{api_key}"
     render json: @response, status: :ok
   end
